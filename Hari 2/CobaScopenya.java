77d6d484187222b46b34fc5ba9ce310b773f9f61
Anggi Anggoro <103877145+Anggi-anggoro@users.noreply.github.com>
2022-08-30 17:05:55 +0700

    Delete Hari 2 directory

--- Hari 2/CobaScopenya.java
+++
@@ -1,8 +0,0 @@
-package com.prodemy.h2;
-
-public class CobaScopenya {
-    public static void main(String[] args) {
-        ScopeContoh scope = new ScopeContoh();
-        scope.firstMethod();
-    }
-}
