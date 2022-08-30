bce6758b3e6a3968fd84381d333f7b5eddf54235
Anggi Anggoro <103877145+Anggi-anggoro@users.noreply.github.com>
2022-08-30 17:04:34 +0700

    Add files via upload

---
+++ Hari 2/CobaScopenya.java
@@ -0,0 +1,8 @@
+package com.prodemy.h2;
+
+public class CobaScopenya {
+    public static void main(String[] args) {
+        ScopeContoh scope = new ScopeContoh();
+        scope.firstMethod();
+    }
+}
